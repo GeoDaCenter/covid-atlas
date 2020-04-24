2e2253e098ccb6135c3d02d3a072d0edf3926908
Robert Martin <rmartin@rmart.in>
2020-04-23 20:51:38 -0500

    Render county and state from app state in data panel

--- src/components/DataPanel.js
+++ src/components/DataPanel.js
@@ -3,13 +3,20 @@ import { connect } from 'react-redux';
 import styles from './DataPanel.module.css';
 
 const DataPanel = (props) => {
+  const { selectedCounty } = props;
+
+  // if there's no selected county, don't render
+  if (!selectedCounty) return null;
+
   return (
     <div className={styles.panel}>
       <header className={styles.header}>
         <h1 className={styles.entityName}>
-          {props.selectedCounty || 'No'} County
+          {props.countyName} County
         </h1>
-        <p className={styles.state}>Alaska</p>
+        <p className={styles.state}>
+          {props.stateName}
+        </p>
       </header>
 
       <p>Data goes here</p>
@@ -19,7 +26,26 @@ const DataPanel = (props) => {
 
 const mapStateToProps = (state) => {
   const { selectedCounty } = state.app;
-  return { selectedCounty };
+
+  let stateName;
+  let countyName;
+
+  if (selectedCounty) {
+    // get state name
+    const { states, counties } = state.datasets['states-and-counties'].data;
+    const selectedCountyAttrs = counties[selectedCounty];
+    const { stateId } = selectedCountyAttrs;
+    stateName = states[stateId].name;
+
+    // get county name
+    countyName = selectedCountyAttrs.name;
+  }
+
+  return {
+    selectedCounty,
+    countyName,
+    stateName
+  };
 };
 
 export default connect(mapStateToProps)(DataPanel);
